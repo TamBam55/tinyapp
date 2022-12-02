9080726898b843a151d574bfc92cf014f74db6fa
Tammy-Lee <tammy-lee.plamondon@telus.com>
2022-12-02 09:06:33 -0800

    updated post function as well as added function to generate random strings for tiny url

--- express_server.js
+++ express_server.js
@@ -2,11 +2,34 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  let x = uuid();
+  return x.substring(0, 6);
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]/
+  res.render("urls_show", templateVars)
+}
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,6 +41,12 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
